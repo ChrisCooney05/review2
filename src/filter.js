6dc28fb4f8eaf83e82d7a55ca9231733ef6e8a12
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-27 14:28:36 +0100

    Able to loop over array and return another array with modified values

--- src/filter.js
+++ src/filter.js
@@ -1,9 +1,13 @@
 function filter(music, lowerBand = 40, upperBand = 1000) {
-  if (music < lowerBand) {
-    return lowerBand;
-  } else {
-    return upperBand;
+  let result = [];
+  for (let i = 0; i < music.length; i++) {
+    if (music[i] < lowerBand) {
+      result.push(lowerBand);
+    } else {
+      result.push(upperBand);
+    }
   }
+  return result;
 }
 
 module.exports = filter;
